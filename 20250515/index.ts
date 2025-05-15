398318d7be61b3ec711ffc837509aac8eeda614f
nakazawaken1 <nakazawaken1@gmail.com>
2025-05-15 11:10:46 +0900

    20250515 ランダムに１つ取り出す

---
+++ 20250515/index.ts
@@ -0,0 +1,13 @@
+// 重複を許可するサンプル
+import sample from './sample';
+const janken = ['グー', 'チョキ', 'パー'];
+const omikuji = ['大吉', '中吉', '小吉', '凶'];
+const atarikuji = ['あたり', 'はずれ'];
+const kuji = ['特等', '1等', '2等', '3等', '4等', '5等'];
+
+const times = (n: number, items: any[]) => [...Array(n)].map(() => sample(items));
+
+console.log(times(3, janken));
+console.log(times(3, omikuji));
+console.log(times(3, atarikuji));
+console.log(times(3, kuji));
